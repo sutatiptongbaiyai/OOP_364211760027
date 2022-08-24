c6af467ce80c4b4c006d7df876e20cf592190af0
su <sutatip.to@rmutsvmail.com>
2022-08-24 16:18:17 +0700

    test upload

---
+++ Assignment/Assignment_1_3.py
@@ -0,0 +1,28 @@
+"""
+Name: {นางสาวสุธาทิพย์ ทองใบใหญ่}
+SID: {364211760027}
+Group: {MIT221}
+"""
+
+"""
+เขียนโปรแกรมไพธอนเพื่อนำเลขบัตรประชาชนมาบวกกันเพื่อทำนายดวงชะตาดังนี้
+• สร้างฟังก์ชันเพื่อหาผลรวมของเลขบัตรประชาชน โดยใช้ชื่อฟังก์ชั่นว่า sumPID()
+• สร้างฟังชั่นชื่อ getFortune() เพื่อทำนายดวงชะตา ถ้าเป็นเลขคู่ให้ทำนายว่า your fortune is good
+• ถ้าเป็นเลขคี่ ให้ทำนายว่า your fortune is very good
+
+ใช้ฟังก์ชั่น split()
+"""
+def getPID():
+   pid = input('Enter your pid: ')  #str
+   lpid = [int(x) for x in pid]
+   return lpid  #list
+
+def getFortune(var):
+        if var % 2 == 0:
+            print('Your fortune is good')
+        else:
+            print('Your fortune is very good')
+
+PID = getPID()
+print(f'The data from user: {sum(PID)}')
+print(f'The prediction result is: {getFortune(sum(PID))}')
