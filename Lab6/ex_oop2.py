f54b47d589eafc32333a5070089832770b862e33
sutatiptongbaiyai <sutatip.to@rmutsvmail.com>
2022-09-07 18:07:57 +0700

    lab6

--- Lab6/ex_oop2.py
+++ Lab6/ex_oop2.py
@@ -7,12 +7,15 @@ Group: {MIT221}
 class Student:
     #class attributes
     uni = "RUTS"
+    my_std = []  # empty list
 
     def __init__(self,name,id,major):
         # object attributes
         self.name = name
         self.id = id
         self.major = major
+        # add object to my_std (list)
+        self.my_std.append(self)
 
     def introduce(self):
         print(f' MY name is {self.name},my id is {self.id}, and I am studying in {self.major} major.')
@@ -33,3 +36,6 @@ Student.uni = "PSU"
 print(s1.uni)
 print(s2.uni)
 
+print(len(Student.my_std))
+
+print(Student.my_std[0].introduce)
