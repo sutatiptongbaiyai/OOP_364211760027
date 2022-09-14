5e077ea88afb957ca94385d56025d6138f6ecc6b
sutatiptongbaiyai <sutatip.to@rmutsvmail.com>
2022-09-14 15:27:57 +0700

    Exam

---
+++ Mid_Exam/mid_Exam.py
@@ -0,0 +1,27 @@
+"""
+Name: {นางสาวสุธาทิพย์ ทองใบใหญ่}
+SID: {364211760027}
+Group: {MIT221}
+"""
+
+class EV_Car:
+    EV_Car = []
+    def __init__(self,brand,model,motor,horsepower,batterysize,range,price):
+        self.brand = brand
+        self.model = model
+        self.motor = motor
+        self.horsepower = horsepower
+        self.batterysize = batterysize
+        self.range = range
+        self.price = price
+        self.EV_Car.append(self)
+
+    def EV_Car_detail(self):
+            print(f'Brand:{self.brand} '
+                  f'Model:{self.model} '
+                  f'Motor:{self.motor} '
+                  f'Horsepower:{self.horsepower} '
+                  f'Batterysize:{self.batterysize} '
+                  f'Range:{self.range} '
+                  f'Price:{self.price} ')
+
