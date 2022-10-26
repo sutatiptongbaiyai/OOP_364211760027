16ab8aa3d36f6b0684927e935e76097bd181c468
suyim <sutatip.to@rmutsvmail.com>
2022-10-26 13:42:56 +0700

    lab10

--- Lab10/model.py
+++ Lab10/model.py
@@ -1,83 +1,108 @@
 """
-Name: {นางสาวสุธาทิพย์ ทองใบใหญ่}
-SID: {364211760027}
-Group: {MIT221}
+Name : {}
+ID: {}
+Group: {}
 """
 class Person:
     def __init__(self,name,age,weight,height):
-        self.__name = name
+        # object attributes
+        self.__name = name # private member
         self.__age = age
         self.__weight = weight
         self.__height = height
-    def __srt__(self):
-        print(f'Name: {self.__name}'
-              f'Age: {self.__age}'
-              f'Weight: {self.__weight}'
-              f'Height: {self.__height}')
-    # getter and setter
+
+    # getter and setter method
     def get_name(self):
-        return  set.name
+        return self.__name
     def set_name(self,name):
         self.__name = name
-
     def get_age(self):
-        return  set.age
-    def get_age(self,age):
+        return self.__age
+    def set_age(self, age):
         self.__age = age
-
     def get_weight(self):
-        return  set.weight
-    def get_weight(self,weight):
+        return self.__weight
+    def set_weight(self, weight):
         self.__weight = weight
-
     def get_height(self):
-        return set.height
-    def get_height(self,height):
+        return self.__height
+    def set_hegiht(self, height):
         self.__height = height
 
+    # display Person object
+    def __str__(self):
+        print(f'\tName: {self.__name}\n'
+                  f'\tAge: {self.__age}\n'
+                  f'\tWeight: {self.__weight}\n'
+                  f'\tHeight: {self.__height}\n')
 
 class Student(Person):
-    def __init__(self,name,age,weight,height,major):
+    def __init__(self,name,age,weight,height,id,major):
         super().__init__(name,age,weight,height)
-        self.major = major
-    def __srt__(self):
-        #super().__srt__()
-        self.person_info()
-        print(f'Major: {self.major}')
+        #Person.__init__(self,name,age,weight,height)
+        self.__id = id
+        self.__major = major
 
+    # getter and setter
+    def get_id(self):
+        return  self.__id
+    def set_id(self,id):
+        self.__id = id
+    def get_major(self):
+        return  self.__major
+    def set_major(self,major):
+        self.__major = major
 
-class Vaccine:
-    all_vaccine = ["Sinovac", "Astrazeneca", "Moderna","Pfizer","Sinopharm"]
+    def __str__(self):
+        print('MT Vaccinated Passport (Student):')
+        super().__str__()
+        print(f'\tSID: {self.__id}\n'
+              f'\tMajor: {self.__major}\n')
+
+class Vaccine():
+
+    all_vaccine = ['Sinovac', 'Astrazeneca', 'Johnson&Johnson', 'Moderna', 'Sinopharm', 'Pfizer']
+
     def __init__(self,vac_name):
         self.__vac_name = vac_name
-    def vaccine_info(self):
-        print(f'vaccine name: {self.__vac_name}')
-    def get_name(self):
-        return self.__vac_name
-    def get_all_vaccine(self):
-            for x in self.all_vaccine:
-                print(x)
 
-class Vaccine_Passport:
-    def __init__(self,owner):
+    #getter and setter method
+    def get_vaccine(self):
+        return self.__vac_name
+    def set_vaccine(self,new_name):
+        self.__vac_name = new_name
+
+    def __str__(self):
+        print(f'Vaccine name: {self.__vac_name}')
+
+class VaccinatedPassport():
+    def __init__(self, owner):
         self.owner = owner
-        self.vaccinated = list()
-        self.date = list()
-    def add_vaccine(self,Vaccine):
-        self.vaccinated.append(Vaccine)
-    def add_date(self,date):
-        self.date.append(date)
-    def vaccine_passport_info(self):
-        print(f'MI Vaccinated Passport:')
-        self.owner.__srt__()
-        c = 1
-        if len(self.vaccinated) == 0:
-            print(f'\t No vaccinated data.')
-        else:
-           # for x in self.vaccinated:
-           #     print(f'{c}. {x.get_name()} dete: {y}')
-            #    c += 1
-            for x in range(len(self.vaccinated)):
-                print(f'{c}. {self.vaccinated[x].get_name()}'
-                      f'date: {self.date[x]}')
-                c += 1
+        self.vaccinated = list() # [] ==> [[v1,'11/7/2564'],[v2,10/10/2564]]
+
+    def add_vaccinated(self,vaccine):
+        self.vaccinated = vaccine
+
+    def __str__(self):
+
+        self.owner.__str__()
+        for x in self.vaccinated:
+            print(f'\tvaccine {self.vaccinated.index(x)+1}: {x[0].get_vaccine()}  \t\tdate: {x[1]}')
+
+class Employee(Person):
+    def __init__(self,name,age,weight,height,position):
+        super().__init__(name,age,weight,height)
+        self.__position = position
+
+    def get_position(self):
+        return self.__position
+    def set_position(self,position):
+        self.__position = position
+
+    def __str__(self):
+        print('MT Vaccinated Passport (Employee):')
+        super().__str__()
+        print(f'\tPosition: {self.__position}\n')
+
+
+print('Hello, from model.')
